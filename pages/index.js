a02f7b046bc98d79144467544e5b32b0d6384c71
rabewtf <rabenklar@gmx.de>
2023-05-15 02:25:58 +0200

    establish connection to api

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,38 @@
+import { useEffect, useState } from "react";
+
 export default function HomePage() {
-  return <p>here will be a super hero app soon</p>;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetch(`https://akabab.github.io/superhero-api/api/all.json`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        setData(actualData);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      {loading && <div>wait a second...</div>}
+
+      {error && <div>(`problem in http: ${error}`)</div>}
+      {console.log("data", data)}
+    </>
+  );
 }
